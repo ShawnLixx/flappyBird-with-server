a3ef06ef8a14444f201da5df00066dc9cbdd6790
root <root@DESKTOP-R0EKBGC.localdomain>
2017-12-15 18:08:34 +0800

    menu updated

--- FlappyBirdClient/lib/game_controller.py
+++ FlappyBirdClient/lib/game_controller.py
@@ -15,7 +15,7 @@ from collision import *
 from netClient import NetClient
 from errors import getErrorString
 import common
-import account
+from account import Account
 
 
 #vars
@@ -63,6 +63,8 @@ def game_start(_gameScene):
     # 给gameScene赋值
     gameScene = _gameScene
     initGameLayer()
+    # log_in_botton = MainMenu()
+    # gameLayer.add(log_in_botton, z=20, name=log_in_botton.name)
     start_botton = SingleGameStartMenu()
     gameLayer.add(start_botton, z=20, name=start_botton.name)
     net.connect()
@@ -178,16 +180,36 @@ class SingleGameStartMenu(Menu):
         self.name = "start_menu"
         self.menu_valign = CENTER
         self.menu_halign = CENTER
+
+        global user
         items = [
                 (ImageMenuItem(common.load_image("button_start.png"), self.gameStart)),
                 (ImageMenuItem(common.load_image("button_notice.png"), showNotice)),
                 (ImageMenuItem(common.load_image("button_difficulty.png"), lambda: chooseDifficulty(self))),
                 ]
+        if user.uid == -1:
+            items.append((ImageMenuItem(common.load_image("button_log_in.png"), self.logIn)))
+            items.append((ImageMenuItem(common.load_image("button_register.png"), self.register)))
+        else:
+            items.append((ImageMenuItem(common.load_image("button_log_out.png"), self.logOut)))
         self.create_menu(items,selected_effect=zoom_in(),unselected_effect=zoom_out())
 
     def gameStart(self):
         gameLayer.remove(self.name)
-        singleGameReady() 
+        singleGameReady()
+
+    def logIn(self):
+        gameLayer.remove(self.name)
+        loginMenu = InputLoginName(self)
+        gameLayer.add(loginMenu, z=70, name=loginMenu.name)
+
+    def register(self):
+        gameLayer.remove(self.name)
+        registerMenu = InputLoginName(self, True)
+        gameLayer.add(registerMenu, z=50, name=registerMenu.name)
+
+    def logOut(self):
+        pass
 
 # Choose difficulty menu and callback functions
 def chooseDifficulty(pre_menu):
@@ -223,5 +245,132 @@ class ChooseDifficultyMenu(Menu):
         print("Set difficuly to " + diff_string + ".")
 
         # back to privious menu
+        gameLayer.remove(self.name)
+        gameLayer.add(self.pre_menu, z=50, name = self.pre_menu.name)
+
+class InputLoginName(cocos.layer.ColorLayer):
+    """Receive the name input here, next is password input"""
+    is_event_handler=True
+    def __init__(self, pre_menu, isRegister=False):
+        super(InputLoginName, self).__init__(0,0,0,0)
+        self.isRegister=isRegister
+        self.name = "login_menu"
+        self.pre_menu = pre_menu
+        self.keys_pressed = ""
+
+        # label = cocos.text.Label('Pick a name:',
+        #     font_name='Courier',
+        #     font_size=24,
+        #     anchor_x='center', anchor_y='center')
+        # label.position = common.visibleSize["width"]/2, common.visibleSize["height"]* 3/4
+        label = createLabel("Input username:\n(End by Enter)", common.visibleSize["width"]/2, common.visibleSize["height"]* 3/4)
+        self.add( label )
+        
+        self.text = createLabel("", common.visibleSize["width"]/2+20, common.visibleSize["height"] * 5/7-50)
+        self.add(self.text)
+
+        self.menu = Menu();
+        self.menu.menu_valign = BOTTOM
+        items = [
+                (ImageMenuItem(common.load_image("button_next.png"), self.next_func)),
+                (ImageMenuItem(common.load_image("button_cancel.png"), self.cancel)),
+                ]
+        self.menu.create_menu(items, selected_effect=zoom_in(), unselected_effect=zoom_out())
+        self.add(self.menu)
+
+    def update_text(self):
+        self.text.element.text = self.keys_pressed
+
+    def on_key_press(self, k, m):
+        if k == pyglet.window.key.ENTER:
+            print "You Entered: {}".format(self.keys_pressed)
+            self.next_func()
+            # cocos.director.director.replace(FadeTransition(main_scene, 1))  # disabled for testing
+            # cocos.director.director.scene.end()  # added for testing
+        else:
+            kk = pyglet.window.key.symbol_string(k)
+            if kk == "SPACE":
+                kk = " "
+            if kk == "BACKSPACE":
+                self.keys_pressed = self.keys_pressed[:-1]
+            else:
+                # ignored_keys can obviously be expanded
+                ignored_keys = ("LSHIFT", "RSHIFT", "LCTRL", "RCTRL", "LCOMMAND", 
+                                "RCOMMAND", "LOPTION", "ROPTION")
+                if kk not in ignored_keys:
+                    self.keys_pressed = self.keys_pressed + kk
+            self.update_text()
+
+    def next_func(self):
+        gameLayer.remove(self.name)
+        passwordButton = InputPassword(self, self.isRegister)
+        gameLayer.add(passwordButton, z=50, name = passwordButton.name)
+
+    def cancel(self):
+        gameLayer.remove(self.name)
+        gameLayer.add(self.pre_menu, z=50, name = self.pre_menu.name)
+
+class InputPassword(cocos.layer.ColorLayer):
+    """Receive the name input here, next is password input"""
+    is_event_handler=True
+    def __init__(self, pre_menu, isRegister=False):
+        super(InputPassword, self).__init__(0,0,0,0)
+        self.isRegister=isRegister
+        self.name = "password_input"
+        self.pre_menu = pre_menu
+        self.keys_pressed = ""
+
+        # label = cocos.text.Label('Pick a name:',
+        #     font_name='Courier',
+        #     font_size=24,
+        #     anchor_x='center', anchor_y='center')
+        # label.position = common.visibleSize["width"]/2, common.visibleSize["height"]* 3/4
+        label = createLabel("Input password:\n(End by Enter)", common.visibleSize["width"]/2, common.visibleSize["height"]* 3/4)
+        self.add( label )
+        
+        self.text = createLabel("", common.visibleSize["width"]/2+20, common.visibleSize["height"] * 5/7-50)
+        self.add(self.text)
+
+        self.menu = Menu();
+        self.menu.menu_valign = BOTTOM
+        items = [
+                (ImageMenuItem(common.load_image("button_next.png"), self.next_func)),
+                (ImageMenuItem(common.load_image("button_cancel.png"), self.cancel)),
+                ]
+        self.menu.create_menu(items, selected_effect=zoom_in(), unselected_effect=zoom_out())
+        self.add(self.menu)
+
+    def update_text(self):
+        self.text.element.text = '*' * len(self.keys_pressed)
+
+    def on_key_press(self, k, m):
+        if k == pyglet.window.key.ENTER:
+            print "You Entered: {}".format(self.keys_pressed)
+            self.next_func()
+            # cocos.director.director.replace(FadeTransition(main_scene, 1))  # disabled for testing
+            # cocos.director.director.scene.end()  # added for testing
+        else:
+            kk = pyglet.window.key.symbol_string(k)
+            if kk == "SPACE":
+                kk = " "
+            if kk == "BACKSPACE":
+                self.keys_pressed = self.keys_pressed[:-1]
+            else:
+                # ignored_keys can obviously be expanded
+                ignored_keys = ("LSHIFT", "RSHIFT", "LCTRL", "RCTRL", "LCOMMAND", 
+                                "RCOMMAND", "LOPTION", "ROPTION")
+                if kk not in ignored_keys:
+                    self.keys_pressed = self.keys_pressed + kk
+            self.update_text()
+
+    def next_func(self):
+        if self.isRegister==False:
+            #Login
+            pass
+        else:
+            #Register
+            pass
+
+    def cancel(self):
         gameLayer.remove(self.name)
         gameLayer.add(self.pre_menu, z=50, name = self.pre_menu.name)
