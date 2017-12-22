bfa0cfecd677ad1b513c62987dd2928a1ad97373
SgLy <775150558@qq.com>
2017-12-22 17:46:22 +0800

    reg and log test

--- FlappyBirdServerTest/main.py
+++ FlappyBirdServerTest/main.py
@@ -9,43 +9,77 @@ from config import HOST, PORT
 import threading
 import unittest
 
+import string
+import random
+def randstr(size = 8):
+    chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(size))
+
 global delay
+size = [10]
 
 from time import time
-def newClient(clients):
+def test(clients, target):
     c = NetClient(host = HOST, port = PORT)
+    clients.append(c)
     start = time()
     c.connect()
+    target(c)
     end = time()
     global delay
     delay += (end - start) * 1000
-    clients.append(c)
+
+def newClient(c):
+    pass
+
+def registerLoginLogout(c):
+    username = randstr()
+    password = randstr()
+    c.registration(username, password)
+    token = c.login(username, password)
+    c.logout(token)
+    token = c.login(username, password)
+    c.logout(token)
+
+def mixedText(c):
+    username = randstr()
+    password = randstr()
+    c.registration(username, password)
+    token = c.login(username, password)
+    notice = c.notice()
+    c.logout()
+
+def testWithPressure(target, prompt):
+    for s in size:
+        print('\nTesting {0} with {1} connections'.format(prompt, s))
+        clients = []
+        threads = []
+        global delay
+        delay = 0
+        for i in range(s):
+            t = threading.Thread( target = test, args = (clients, target) )
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
+        print('  Average delay: {0} ms'.format(delay / s))
 
 class Test(unittest.TestCase):
-    def test_connection(self):
+    def test_00_connection(self):
         print('\nTesting connection')
         global delay
         delay = 0
         clients = []
-        newClient(clients)
+        test( clients, newClient )
         print('  Delay: {0} ms'.format(delay))
         self.assertTrue(clients[0].connected)
 
-    def test_manyConnection(self):
-        size = [10, 20]
-        for s in size:
-            print('\nTesting {0} connections'.format(s))
-            clients = []
-            threads = []
-            global delay
-            delay = 0
-            for i in range(s):
-                t = threading.Thread( target = newClient, args = (clients, ) )
-                t.start()
-                threads.append(t)
-            for t in threads:
-                t.join()
-            print('  Average delay: {0} ms'.format(delay / s))
+    def test_01_manyConnection(self):
+        testWithPressure( newClient, 'connection' )
+        self.assertTrue(True)
+
+    def test_02_RegisterLoginLogout(self):
+        testWithPressure( registerLoginLogout, 'register, login and logout')
         self.assertTrue(True)
 
 if __name__ == '__main__':
